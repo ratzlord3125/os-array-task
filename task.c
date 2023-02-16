46b8a541794102b6abc7b03ff939a47a37be9a0c
ratzlord3125 <f20200624@hyderabad.bits-pilani.ac.in>
2023-02-16 21:43:07 +0530

    minor ch

--- task.c
+++ task.c
@@ -7,6 +7,7 @@
 #include <stdlib.h> 
 #include <pthread.h>
 #include <signal.h>
+#include <./CSF372.h>>
 
 int processReturn = 0;
 int rowSum = 0;
@@ -23,7 +24,8 @@ int processRow(int* arr, int n) {
     pthread_t tid; 
     pthread_attr_t attr; 
     pthread_t workers[n]; 
-    for(int i=0; i<n; i++) {
+    for(int i=0; i<n; i++) 
+    {
         // printf("Creating thread (ptid :: %ld)\n", tid);
         pthread_create(&tid, NULL, averageCalc, &arr[i]); 
         workers[i] = tid; 
@@ -42,37 +44,51 @@ void sigchld() {
         printf("Child exited successfully\n");
     }
 }
+
 int main(int argc, char**argv)
 {
+    // taking values according to input format
+    int n = atoi(argv[1]);
+    int a = atoi(argv[2]);
+    int b = atoi(argv[3]);
+    int p = atoi(argv[4]); 
 
-    int n = atoi(argv[1]), a = atoi(argv[2]), b = atoi(argv[3]), p = atoi(argv[4]); 
-    int arr[n][n]; 
+    int arr[n][n]; // to store the input in matrix 
 
-    printf("n : %d | a : %d | b : %d | p : %d\n", n, a, b, p); 
-    printf("The entered matrix : \n");
-    for(int i=0; i<n*n; i++) {
+    printf("---INPUT---");
+    printf("n : %d \n a : %d \n b : %d \n p : %d\n", n, a, b, p); 
+    printf("Matrix : \n");
+    for(int i=0; i<n*n; i++) 
+    {
         if((i)%n == 0) putchar('\n');
-        printf("%s ", argv[i+5]);
         arr[i/n][i%n] = atoi(argv[i+5]); 
+
+        printf("%d ", arr[i/n][i%n]);
     }
     putchar('\n');
-    int pi[n*2]; 
+
     //forking n processes
     int controller_pid = getpid(); 
+    int pi[n*2]; 
     signal(SIGCHLD, sigchld);
     printf("Forking %d processes for the parent process (pid :: %d)\n", n, controller_pid); 
-    for(int i=0; i<n; i++) {
+    for(int i=0; i<n; i++) 
+    {
         int pid = getpid(); 
         pipe(pi+i*2);
-        if(pid == controller_pid) {
+        if(pid == controller_pid) 
+        {
             int child_pid = fork(); 
-            if(child_pid) { //you are in parent
+            if(child_pid)  //you are in parent
+            {
                 printf("Forked child (pid :: %d) for parent (pid :: %d)\n", child_pid, getppid()); 
                 int x;
                 wait(&processReturn);
                 read(*(pi+i*2), &x, sizeof(int)); 
                 printf("Read value %d from parent \n", x);
-            } else { //you are in child
+            } 
+            else  //you are in child
+            {
                 printf("Processing row :: %d : ", i); 
                 int x = processRow(arr[i], n); 
                 write(*(pi+i*2+1), &x, sizeof(int)); 
@@ -83,5 +99,5 @@ int main(int argc, char**argv)
     }
     return 0;
 }
-//4 10 99 5 17 28 67 65 22 19 11 77 89 78 45 40 20 10 90 76
+
 // gcc task.c -pthread -o task && ./task 10 99 5 17 28 67 65 22 19 11 77 89 78 45 40 20 10 90 76
